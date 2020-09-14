7d59b86ac6d058c8a4a135cf4c89266d6e0372bf
carlos molero <cmolero130@gmail.com>
2020-09-14 18:15:38 -0400

    created a write head

--- test/server.js
+++ test/server.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function (req, res){
-    res.end()
-}).listen(3000, function() {
-    console.log('Listen for request on Port 3000')
-})
+http.createServer(function (req, res) {
+    if (req.url === "/OK") {
+        console.log("Inbound 'OK request being processed...");
+        res.writeHead(200);
+        res.end();
+    } else {
+        console.log("this is an error...");
+        res.writeHead(404);
+        res.end();
+    }
+}).listen(3000, function () {
+    console.log("Listen for request on Port 3000");
+});
